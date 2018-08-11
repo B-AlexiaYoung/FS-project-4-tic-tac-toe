562bf3916d3e8ebba0f119277dcba880647c1b1c
B-AlexiaYoung <brlxyng@gmail.com>
2018-08-11 16:15:05 -0600

    reset working and tidy

--- js/main.js
+++ js/main.js
@@ -6,34 +6,19 @@ const start = document.getElementById("start");
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 const boxes = document.getElementsByClassName("boxes");
-const box1 = document.getElementById("box1");
-const box2 = document.getElementById("box2");
-const box3 = document.getElementById("box3");
-const box4 = document.getElementById("box4");
-const box5 = document.getElementById("box5");
-const box6 = document.getElementById("box5");
-const box7 = document.getElementById("box7");
-const box8 = document.getElementById("box8");
-const box9 = document.getElementById("box9");
-console.log(box1);
+const box = document.getElementsByClassName("box");
+const newgame = document.getElementById("new");
+const finish = document.getElementById("finish");
 const chosenBoxTracker = [];
 let player1ChosenBox = [];
 let player2ChosenBox = [];
 let counter = 0;
-
-
-
+let chosenBox;
+const boxli = document.getElementsByTagName("li");
+const players = document.getElementsByClassName("players")
 //hide board at start of game
 board.style.display = "none";
-
-//listener on button to remove start screen and reveal gameboard
-startButton[0].addEventListener("click", (event) => {
-    start.style.display = "none";
-    board.style.display = "block";
-})
-
-//make start player active,  O is player 1;
-player1.classList.add("class", "active");
+finish.style.display = "none";
 
 //===============================================
 //                  LISTENERS
@@ -43,150 +28,211 @@ player1.classList.add("class", "active");
 startButton[0].addEventListener("click", (event) => {
     start.style.display = "none";
     board.style.display = "block";
-
-
+    player1.classList.add("class", "active");
 });
-let tracknoughtsandcrosses = true;
+for (let m = 2; m < boxli.length; m++) {
+    boxli[m].addEventListener("mouseenter", enterListener);
+}
 
-//listener on boxes for gameplay (on the ul)
-boxes[0].addEventListener("click", (event) => {
-    console.log(tracknoughtsandcrosses);
-    if (tracknoughtsandcrosses === true) {
-        tracknoughtsandcrosses = noughts(event);
-    } else if (tracknoughtsandcrosses === false) {
-        tracknoughtsandcrosses = crosses(event);
+function enterListener(currentTarget) {
+    let hoverbox = event.currentTarget;
+    if (player1.classList.contains("active")) {
+        hoverbox.style.backgroundImage = "url(./img/o.svg)";
+        hoverbox.style.backgroundImage.display = "block";
+    } else if (player2.classList.contains("active")) {
+        hoverbox.style.backgroundImage = "url(./img/x.svg)";
+        hoverbox.style.backgroundImage.display = "block";
     }
-
-    if (counter >= 3) {
-        checkWinDraw();
-    }
+}
+//listener to replay
+newgame.addEventListener("click", (event) => {
+    reset();
 })
+//add individual event listener so that individual ones can be removed
+for (let n = 2; n < boxli.length; n++) {
+    boxli[n].addEventListener("mouseleave", leaveListener);
+}
 
-
+function leaveListener(currentTarget) {
+    let hoverbox = event.currentTarget;
+    if (player1.classList.contains("active")) {
+        if (!hoverbox.classList.contains("box-filled-1") || !hoverbox.classList.contains("box-filled-2")) {
+            hoverbox.style.backgroundImage.display = "none";
+            hoverbox.style.backgroundImage = "";
+        }
+    } else if (player2.classList.contains("active")) {
+        if (!hoverbox.classList.contains("box-filled-1") || !hoverbox.classList.contains("box-filled-2")) {
+            console.log(!hoverbox.classList.contains("box-filled-1"));
+            console.log(!hoverbox.classList.contains("box-filled-2"));
+            hoverbox.style.backgroundImage.display = "none";
+            hoverbox.style.backgroundImage = "";
+        }
+    }
+}
+let tracknoughtsandcrosses = true;
+//listener on boxes for gameplay (on the ul)
+for (let k = 2; k < boxli.length; k++) {
+    boxli[k].addEventListener("click", (event) => {
+        if (tracknoughtsandcrosses === true) {
+            tracknoughtsandcrosses = noughts(event);
+        } else if (tracknoughtsandcrosses === false) {
+            tracknoughtsandcrosses = crosses(event);
+        }
+        if (counter >= 5) {
+            checkWinDraw();
+        }
+    })
+}
 //helper for boxes listener
-
 const noughts = (event) => {
-    console.log(event.target);
-    counter = counter + 1;
-    console.log(counter);
-    let chosenBox = event.target;
-    player1.classList = "active";
-    console.log(chosenBox.classList.contains("box-filled-2"));
-    console.log(chosenBox.classList.contains("box-filled-1"));
-
+    chosenBox = event.target;
+    player1.classList.add("active");
     if (chosenBox.classList.contains("box-filled-2") || chosenBox.classList.contains("box-filled-1")) {
-        
         return true;
     } else {
-        chosenBox.classList.add( "box-filled-1")
-       
+        chosenBox.classList.add("box-filled-1")
+        chosenBox.removeEventListener("mouseenter", enterListener);
+        chosenBox.removeEventListener("mouseleave", leaveListener);
         boxnum = chosenBox.getAttribute("id");
-
-        if (chosenBox.classList.contains("box-filled-2") === false /*|| chosenBox.classList.contains("box-filled-1")===true*/ ) {
-            console.log(chosenBox.classList.contains("box-filled-2"+ "no boxfilled 2"));
-            console.log(chosenBox.classList.contains("box-filled-1" + "no boxfilled 1"));
-
-            console.log(player1ChosenBox.indexOf(boxnum));
-
+        if (chosenBox.classList.contains("box-filled-2") === false) {
             if (player1ChosenBox.indexOf(boxnum) === -1) {
                 player1ChosenBox.push(boxnum);
+                counter = counter + 1;
             }
         }
-        console.log(event.target);
         player1.classList.remove("active");
-        console.log(event.target);
         player2.classList.add("active");
-        console.log(event.target);
-
         return false;
-        //show player 1 symbol
-        // push player 1 to box Id box1
     }
 }
 
-
-
 const crosses = (event) => {
-    let chosenBox = event.target;
-    counter = counter + 1;
-    console.log(counter+ "counter");
-    player2.classList = "active";
+    chosenBox = event.target;
+    player2.classList.add("active");
     if (chosenBox.classList.contains("box-filled-2") || chosenBox.classList.contains("box-filled-1")) {
         return false;
     } else {
-
         chosenBox.classList.add("box-filled-2")
-        
+        chosenBox.removeEventListener("mouseenter", enterListener);
+        chosenBox.removeEventListener("mouseleave", leaveListener);
         boxnum = chosenBox.getAttribute("id");
-       
-        if (chosenBox.classList.contains("box-filled-1") === false /*|| chosenBox.contains("box-filled-2")===true */ ) {
-            console.log(chosenBox.classList.contains("box-filled-1"));
-            console.log(player2ChosenBox.indexOf(boxnum));
+        if (chosenBox.classList.contains("box-filled-1") === false) {
             if (player2ChosenBox.indexOf(boxnum) === -1) {
-
                 player2ChosenBox.push(boxnum);
+                counter = counter + 1;
             }
         }
-        console.log(event.target);
-
         player2.classList.remove("active");
-        console.log(event.target);
-
         player1.classList.add("active");
-        console.log(event.target);
-
-        console.log(player2ChosenBox + "player2 chosen boxes");
-       
         return true;
     }
 }
 
 // funtion to check for win or draw
 const checkWinDraw = () => {
-
-
     // check for win
-    let monkey = player1ChosenBox.includes("box1", "box2", "box3");
-    console.log(monkey);
-//player 1 win
+    //player 1 win
     if (player1ChosenBox.includes("box1") && player1ChosenBox.includes("box2") && player1ChosenBox.includes("box3")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box1") && player1ChosenBox.includes("box4") && player1ChosenBox.includes("box7")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box1") && player1ChosenBox.includes("box5") && player1ChosenBox.includes("box9")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box4") && player1ChosenBox.includes("box5") && player1ChosenBox.includes("box6")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box2") && player1ChosenBox.includes("box5") && player1ChosenBox.includes("box8")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box3") && player1ChosenBox.includes("box6") && player1ChosenBox.includes("box9")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box7") && player1ChosenBox.includes("box8") && player1ChosenBox.includes("box9")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } else if (player1ChosenBox.includes("box7") && player1ChosenBox.includes("box5") && player1ChosenBox.includes("box3")) {
-        console.log("player 1 wins");
+        tracknoughtsandcrosses = true;
+        playerWin(tracknoughtsandcrosses);
     } //        player 2 win            ==========================
     else if (player2ChosenBox.includes("box1") && player2ChosenBox.includes("box2") && player2ChosenBox.includes("box3")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box1") && player2ChosenBox.includes("box4") && player2ChosenBox.includes("box7")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box1") && player2ChosenBox.includes("box5") && player2ChosenBox.includes("box9")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box4") && player2ChosenBox.includes("box5") && player2ChosenBox.includes("box6")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box2") && player2ChosenBox.includes("box5") && player2ChosenBox.includes("box8")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box3") && player2ChosenBox.includes("box6") && player2ChosenBox.includes("box9")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box7") && player2ChosenBox.includes("box8") && player2ChosenBox.includes("box9")) {
-        console.log("player 2 wins");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
     } else if (player2ChosenBox.includes("box7") && player2ChosenBox.includes("box5") && player2ChosenBox.includes("box3")) {
-        console.log("player 2 wins");
-    }else{
-        console.log("draw");
+        tracknoughtsandcrosses = false;
+        playerWin(tracknoughtsandcrosses);
+    } else if (player1ChosenBox.length + player2ChosenBox.length === 9) {
+        tracknoughtsandcrosses = null;
+        playerWin(tracknoughtsandcrosses);
+    } else {
+        console.log("keep playing");
     }
-
 }
-
-
-// needs a mouseover listener to show their symbol
+//player 1 win screen
+const playerWin = (tracknoughtsandcrosses) => {
+    board.style.display = "none";
+    finish.style.display = "block";
+    finish.classList.add("screen");
+    finish.classList.add("screen-win");
+    let message = document.getElementById("message");
+    if (tracknoughtsandcrosses === true) {
+        finish.classList.add("screen-win-one");
+        message.textContent = "Player 1 wins!";
+    } else if (tracknoughtsandcrosses === false) {
+        finish.classList.add("screen-win-two");
+        message.textContent = "Player 2 wins!";
+    } else if (tracknoughtsandcrosses === null) {
+        finish.classList.add("screen-win-tie");
+        message.textContent = "It's a draw!";
+    }
+}
+// reset board
+const reset = () => {
+    player1.classList.remove("active");
+    player2.classList.remove("active");
+    if (finish.classList.contains("screen-win-one")) {
+        finish.classList.remove("screen-win-one");
+    }
+    if (finish.classList.contains("screen-win-two")) {
+        finish.classList.remove("screen-win-two");
+    }
+    if (finish.classList.contains("screen-win-tie")) {
+        finish.classList.remove("screen-win-tie")
+    }
+    finish.style.display = "none";
+    start.style.display = "block";
+    tracknoughtsandcrosses = true;
+    chosenBoxTracker.length = 0;
+    player1ChosenBox.length = 0;
+    player2ChosenBox.length = 0;
+    counter = 0;
+    for (t = 0; t < box.length; t++) {
+        if (box[t].classList.contains("box-filled-1") || box[t].classList.contains("box-filled-2")) {
+            box[t].style.backgroundImage = "none";
+            box[t].addEventListener("mouseenter", enterListener);
+            box[t].addEventListener("mouseleave", leaveListener);
+            box[t].classList.remove("box-filled-1");
+            box[t].classList.remove("box-filled-2");
+        }
+    }
+}
